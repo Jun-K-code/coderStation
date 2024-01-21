e7fb0b7c5d5c8d02318d04b1c0e5dffe177ffecd
Jun-K-code <JunCode@aliyun.com>
2024-01-21 18:58:57 +0800

    fix: Fe-路由-配置我要发问内容页面路由

--- Fe/src/router/index.tsx
+++ Fe/src/router/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Issues from '../pages/Issues';
 import Books from '../pages/Books';
 import Interviews from '../pages/Interviews';
+import AddIssue from '../pages/AddIssue';
 
 const RouteConfig = () => {
     return (
@@ -10,6 +11,7 @@ const RouteConfig = () => {
             <Route path="/issues" element={<Issues />} />
             <Route path="/books" element={<Books />} />
             <Route path="/interviews" element={<Interviews />} />
+            <Route path="/addIssue" element={<AddIssue />} />
             <Route path="/" element={<Navigate to='/issues' replace />} />
         </Routes>
     );
