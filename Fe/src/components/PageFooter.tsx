76e9145a709b1f5030ef38ae0ac7198fe94e57c7
Jun-K-code <JunCode@aliyun.com>
2024-01-22 20:04:40 +0800

    fix: 修改主页footer页脚

--- Fe/src/components/PageFooter.tsx
+++ Fe/src/components/PageFooter.tsx
@@ -5,33 +5,11 @@ export const PageFooter = () => {
         <div>
             <p className="links">
                 <span className="linkItem">友情链接：</span>
-                <a
-                    href="http://www.yuanjin.tech/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="linkItem"
-                >
-                    袁进的博客
-                </a>
-                <a
-                    href="http://yanhongzhi.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="linkItem"
-                >
-                    Mr.Yan
-                </a>
-                <a
-                    href="https://blog.csdn.net/jackfrued"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="linkItem"
-                >
-                    骆昊的技术专栏
+                <a href="https://juejin.cn/" target="_blank" rel="noreferrer" className="linkItem">
+                    掘金
                 </a>
             </p>
-            <p>© 2022 - Coder Station</p>
-            <p>Powered by Create React App</p>
+            <p>© 2024 - Coder Station</p>
         </div>
     );
 };
